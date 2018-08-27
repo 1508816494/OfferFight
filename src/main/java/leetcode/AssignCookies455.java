7e3483f4a701f4916a79bf6e123708ae2eff3206
yanting <1508816494@qq.com>
2018-08-27 16:37:24 +0800

    isSubSequence

--- src/main/java/leetcode/AssignCookies455.java
+++ src/main/java/leetcode/AssignCookies455.java
@@ -4,35 +4,33 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class AssignCookies455 {
-    private Integer[] g;
-    private Integer[] s;
+//    private Integer[] g;
+//    private Integer[] s;
 
     public int findContentChildren(int[] g, int[] s) {
-//        this.g = g;
-//        this.s = s;
-        this.g = new Integer[g.length];
-        for(int i = 0; i < g.length; i++) {
-            this.g[i] = g[i];
-        }
-        this.s = new Integer[s.length];
-        for(int i = 0; i < s.length; i++) {
-            this.s[i] = s[i];
-        }
+//        this.g = new Integer[g.length];
+//        for(int i = 0; i < g.length; i++) {
+//            this.g[i] = g[i];
+//        }
+//        this.s = new Integer[s.length];
+//        for(int i = 0; i < s.length; i++) {
+//            this.s[i] = s[i];
+//        }
 
-        Arrays.sort(this.g, Collections.reverseOrder());
-        Arrays.sort(this.s, Collections.reverseOrder());
+        Arrays.sort(g);
+        Arrays.sort(s);
 
-        int gi = 0;
-        int si = 0;
+        int gi = g.length - 1;
+        int si = s.length - 1;
         int res = 0;
-        while (gi < this.g.length && si < this.s.length) {
+        while (gi >= 0 && si >= 0) {
             //当前最大的饼干可以满足当前最贪心的小朋友
-            if(this.s[si] >= this.g[gi]) {
+            if(s[si] >= g[gi]) {
                 res++;
-                si++;
-                gi++;
+                si--;
+                gi--;
             } else {
-                gi++;
+                gi--;
             }
         }
         return res;
